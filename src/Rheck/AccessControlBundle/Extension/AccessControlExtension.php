df9410bb6623764f18a9943eff4ac0e1ed1aa36d
Ricardo Heck <ricardoh3ck@gmail.com>
2013-07-28 01:02:14 -0300

    Added AccessControlExtension to check the permissions on the html view

---
+++ src/Rheck/AccessControlBundle/Extension/AccessControlExtension.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Rheck\AccessControlBundle\Extension;
+
+use Rheck\AccessControlBundle\Service\AccessControlService;
+
+class AccessControlExtension
+{
+    protected $accessControlService;
+
+    public function __construct(AccessControlService $accessControlService)
+    {
+        $this->accessControlService = $accessControlService;
+    }
+
+    public function getFilters()
+    {
+        return array();
+    }
+
+    public function getFunctions()
+    {
+        return array(
+            'permissionAccess' => new \Twig_Function_Method($this, 'permissionAccess'),
+        );
+    }
+
+    public function getName()
+    {
+        return 'accessControlExtension';
+    }
+
+    public function permissionAccess(
+        $permissions = array(),
+        $context     = null,
+        $criteria    = "AND",
+        $strategy    = "rheck.access_control.default.strategy"
+    ) {
+        return $this->accessControl->checkPermission($permissions, $context, $criteria, $strategy);
+    }
+}
