5c78f91df9470fc35d5e4ed383d034dae0adf069
Ricardo Heck <ricardoh3ck@gmail.com>
2013-07-29 09:42:00 -0300

    Fixed the message of Exception

--- Exception/StrategyNotFoundException.php
+++ Exception/StrategyNotFoundException.php
@@ -6,7 +6,7 @@ class StrategyNotFoundException extends \Exception
 {
     public function __construct($id, \Exception $previous = null)
     {
-        $msg  = 'You have request a non-service strategy';
+        $msg  = 'You have request a non-service strategy. ';
         $msg .= sprintf('You have sure that you have registered your strategy "%s" as service?', $id);
 
         parent::__construct($msg, 0, $previous);
