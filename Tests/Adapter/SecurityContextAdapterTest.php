a755162c2ee2a8c2b2f01fbe9647fa32b22abdfe
Ricardo Heck <ricardoh3ck@gmail.com>
2013-08-01 22:23:29 -0300

    Fixed the security context adapter test

--- Tests/Adapter/SecurityContextAdapterTest.php
+++ Tests/Adapter/SecurityContextAdapterTest.php
@@ -6,10 +6,8 @@ use Rheck\AccessControlBundle\Adapter\SecurityContextAdapter;
 
 class SecurityContextAdapterTest extends \PHPUnit_Framework_TestCase
 {
-    public function testGetLogged()
+    private function getTokenMock($userMock)
     {
-        $userMock = $this->getMock('Symfony\Component\Security\Core\User\UserInterface');
-
         $tokenMock = $this->getMock('Symfony\Component\Security\Core\Authentication\Token\TokenInterface');
         $tokenMock->expects($this->any())
             ->method('isAuthenticated')
@@ -18,20 +16,34 @@ class SecurityContextAdapterTest extends \PHPUnit_Framework_TestCase
             ->method('getUser')
             ->will($this->returnValue($userMock));
 
+        return $tokenMock;
+    }
+
+    private function getSecurityContextMock($tokenMock)
+    {
         $securityContextMock = $this->getMock('Symfony\Component\Security\Core\SecurityContextInterface');
         $securityContextMock->expects($this->any())
             ->method('getToken')
             ->will($this->returnValue($tokenMock));
 
+        return $securityContextMock;
+    }
+
+    public function testGetLogged()
+    {
+        $userMock = $this->getMock('Symfony\Component\Security\Core\User\UserInterface');
+        $tokenMock = $this->getTokenMock($userMock);
+        $securityContextMock = $this->getSecurityContextMock($tokenMock);
+
         $securityContextAdapter = new SecurityContextAdapter();
         $securityContextAdapter->setSecurityContext($securityContextMock);
+
+        $loggedUser = $securityContextAdapter->getLogged();
 
         $this->assertInstanceOf(
             'Symfony\Component\Security\Core\SecurityContextInterface',
             $securityContextAdapter->getSecurityContext()
         );
-
-        $loggedUser = $securityContextAdapter->getLogged();
 
         $this->assertInstanceOf(
             'Symfony\Component\Security\Core\User\UserInterface',
